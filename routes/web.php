d243971115f99395ff776f4d024ad590f1891f5d
Pedro Salemi <pedrosalemi@gmail.com>
2026-06-16 21:27:58 -0300

    Apenas apagando algumas rotas comentadas. Sigo no desenvolvimento do mobile e NÃO esqueci que ainda preciso finalizar o web (as telas).

--- routes/web.php
+++ routes/web.php
@@ -5,11 +5,6 @@ $router->post('/games','GameController@store')->middleware('auth')->middleware('
 $router->post('/games/delete', 'GameController@delete')->middleware('csrf');
 $router->get('/games/edit', 'GameController@edit');
 $router->post('/games/update', 'GameController@update')->middleware('csrf');
-//$router->get('/api/games', 'ApiGameController@index');
-// $router->post('/api/games', 'ApiGameController@store');
-// $router->get('/api/games/{id}', 'ApiGameController@show');
-// $router->put('/api/games/{id}', 'ApiGameController@update');
-// $router->delete('/api/games/{id}', 'ApiGameController@destroy');
 $router->get('/register', 'AuthController@showRegister');
 $router->post('/register', 'AuthController@register')->middleware('csrf');
 $router->get('/login', 'AuthController@showLogin');
